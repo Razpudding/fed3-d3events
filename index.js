2107eecfec2c5580f01a9a97a91e1a9c106eef33
Laurens <laurens.aarnoudse@gmail.com>
2017-10-01 00:50:32 +0200

    Adding example files

---
+++ index.js
@@ -0,0 +1,118 @@
+
+d3.json("eventData.json", function(error, data) {
+	if (error) { throw error }
+		let titles = data.map(event => event.title)
+	console.log(titles)
+	console.log(data[0]["location"]["latitude"])
+
+	console.table(data.splice(data.length -10))
+});
+
+
+// Set the stage
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+width = 960 - margin.left - margin.right,
+height = 500 - margin.top - margin.bottom;
+
+var x = d3.scaleLinear()
+.range([0, width]);
+
+var y = d3.scaleLinear()
+.range([height, 0]);
+
+var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+var xAxis = d3.axisBottom(x)
+
+var yAxis = d3.axisLeft(y)
+
+var svg = d3.select("body").append("svg")
+.attr("width", width + margin.left + margin.right)
+.attr("height", height + margin.top + margin.bottom)
+.append("g")
+.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.json("eventData.json", function(error, data) {
+	//turn on the next line if you want to work with a small data test set
+	//data = data.splice(data.length - 10)
+	if (error) throw error;
+
+	data.forEach(function(d) {
+		if (d["location"]["latitude"] == NaN){
+			console.log("empty one")
+			d.lat = 52,3780870
+			d.lng = 4,9011690
+		}
+  	//else { console.log(d["location"]["latitude"])}
+  	d.lat = parseFloat(d["location"]["latitude"].replace(',', '.'));
+  	d.lng = parseFloat(d["location"]["longitude"].replace(',', '.'));
+  	console.log(d.lat, d.lng)
+  });
+
+	x.domain(d3.extent(data, function(d) { return d.lng; }));
+	y.domain(d3.extent(data, function(d) { return d.lat; }));
+	console.log(x.domain(), y.domain())
+	svg.append("g")
+	.attr("class", "x axis")
+	.attr("transform", "translate(0," + height + ")")
+	.call(xAxis)
+	.append("text")
+	.attr("class", "label")
+	.attr("x", width)
+	.attr("y", -6)
+	.style("text-anchor", "end")
+	.text("Sepal Width (cm)");
+
+	svg.append("g")
+	.attr("class", "y axis")
+	.call(yAxis)
+	.append("text")
+	.attr("class", "label")
+	.attr("transform", "rotate(-90)")
+	.attr("y", 6)
+	.attr("dy", ".71em")
+	.style("text-anchor", "end")
+	.text("Sepal Length (cm)")
+
+	svg.selectAll(".dot")
+	.data(data)
+	.enter().append("circle")
+	.attr("class", "dot")
+	.attr("r", 3.5)
+	.attr("cx", function(d) { return x(d.lng); })
+	.attr("cy", function(d) { return y(d.lat); })
+	.style("fill", function(d) { return color(d.last); });
+
+	var legend = svg.selectAll(".legend")
+	.data(color.domain())
+	.enter().append("g")
+	.attr("class", "legend")
+	.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+	legend.append("rect")
+	.attr("x", width - 18)
+	.attr("width", 18)
+	.attr("height", 18)
+	.style("fill", color);
+
+	legend.append("text")
+	.attr("x", width - 24)
+	.attr("y", 9)
+	.attr("dy", ".35em")
+	.style("text-anchor", "end")
+	.text(function(d) { return d; });
+
+});
+
+
+
+
+
+/* BAsics:
+const url = "https://open.data.amsterdam.nl/Attracties.json"
+
+d3.json(url, function(error, data) {
+  	console.dir(data)
+  	console.table(data)
+});
+*/
---
+++ readme.md
@@ -0,0 +1,19 @@
+# This repo functions as an example of how you can load an
+online JSON file with D3 and how you can visualize it
+
+## Data
+The data used comes from the [municipality of Amsterdam](https://data.amsterdam.nl/#?dte=catalogus%2Fapi%2F3%2Faction%2Fpackage_show%3Fid%3Dd7a4c93c-0d7f-4d39-82d4-5f50eaffa624&dtfs=T&dsf=res_format::JSON&mpb=topografie&mpz=11&mpv=52.3731081:4.8932945)
+It includes a list of events and sights which updates
+ daily, providing a great example of a changing dataset
+  for students
+
+The dataset holds for each event the following information:
+* title 
+* short Description
+* lastupdated: Last updated timestamp
+* types: _object_ holding at least one type and category id
+* media: an _array_ holding 0 or more objects whhich hold
+a url to an image of the event
+* location: _object_ With a name, city, adress, zipcode and lat long
+The list above is of datatype string unless mentioned otherwise
+
