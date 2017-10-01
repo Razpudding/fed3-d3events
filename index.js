b853a265aadffe4cf3ce2ca125a4fa38f1d61f50
Laurens <laurens.aarnoudse@gmail.com>
2017-10-01 12:50:24 +0200

    I tried to make sense of each items category and mapped the color to the corresponding category. Also started adding the steps I take to make this datavis to the readme

--- index.js
+++ index.js
@@ -1,14 +1,64 @@
+//I'm using this global variable in case I want to expose any part of the data
+// To the global scope so I can debug it in the browser console on the go
+var glob;
 
+
+
+// We'll use the code below to examine and experiment with the data
+// This code works with a subset of the data (using splice) which causes it to
+// run faster and makes the print outs easier to read.
+// If one of the tests works, we simply copy the logic to the main code below it
 d3.json("eventData.json", function(error, data) {
 	if (error) { throw error }
-		let titles = data.map(event => event.title)
-	console.log(titles)
-	console.log(data[0]["location"]["latitude"])
+	//Make a selection of the data
+	glob = data[0]
+	//data.splice(500)	//First x elements
+	//If you turn the forEach into a map you can chain the sort after it
+	// By using dot notation :)
+	data.forEach((d) => {
+		let subcats = d["types"][0]["catid"].split(".")
+		d.code = subcats[0] + subcats[1] + "," + subcats[2]
+	})
+	data.sort(function(x, y){
+	  	return d3.ascending(x.code, y.code);
+	})
+	data.forEach((d) => {
+		if (d.code == "22,19"){
+			//console.log(d)
+		}
+		console.log(d["title"], d.code)
+	})
+	//console.table(data)
 
-	console.table(data.splice(data.length -10))
+
 });
 
+const types = {
+	"21,1" : "Activiteiten",
+	"21,11" : "Zwembaden",
+	"21,2" : "Wellness",
+	//"21,3" : "Casinos",
+	//"21,4" : "Dierentuinen",
+	"21,5" : "Boererijen",
+	"21,6" : "Musea",
+	"21,7" : "Recreatie gebieden",
+	"21,8" : "Rondvaarten",
+	"22,1" : "verzameling, openbare kunst",
+	"22,10" : "Natuur",
+	"22,11" : "Parken",
+	"22,12" : "(Stads)Stranden",
+	"22,16" : "Forten",
+	"22,18" : "Bossen",
+	"22,19" : "Willekeurig?",
+	"22,3" : "Bezoekers centra",
+	"22,4" : "Gallerieën",
+	"22,6" : "Buitenplaatsen",
+	"22,7" : "Kerken",
+	"22,8" : "Molens",
+	"22,9" : "Monumenten",
+}
 
+// This chart is based off of: https://bl.ocks.org/mbostock/3887118
 // Set the stage
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
 width = 960 - margin.left - margin.right,
@@ -20,7 +70,8 @@ var x = d3.scaleLinear()
 var y = d3.scaleLinear()
 .range([height, 0]);
 
-var color = d3.scaleOrdinal(["orange","grey","black"]);
+//var color = d3.scaleOrdinal(["orange","grey","black"]);
+var color = d3.scaleOrdinal(d3.schemeCategory20)
 
 var xAxis = d3.axisBottom(x)
 
@@ -36,7 +87,8 @@ d3.json("eventData.json", function(error, data) {
 	//turn on the next line if you want to work with a small data test set
 	//data = data.splice(data.length - 10)
 	if (error) throw error;
-
+	//Let's rewrite the data a bit to more usable notation
+	//First we write the latlng info directly to the root of the object
 	data.forEach(function(d) {
 		if (d["location"]["latitude"] == NaN){
 			console.log("empty one")
@@ -45,8 +97,13 @@ d3.json("eventData.json", function(error, data) {
 		}
   		d.lat = parseFloat(d["location"]["latitude"].replace(',', '.'));
   		d.lng = parseFloat(d["location"]["longitude"].replace(',', '.'));
-	});
+  		//Next, we'll write our own version of categories to the root of the object
+  		let subcats = d["types"][0]["catid"].split(".")
+		d.code = subcats[0] + subcats[1] + "," + subcats[2]
 
+	});
+	// Now that the data has been reformatted to our liking we can calculate the domains
+	// For our axes
 	x.domain(d3.extent(data, function(d) { return d.lng; }));
 	y.domain(d3.extent(data, function(d) { return d.lat; }));
 	svg.append("g")
@@ -79,6 +136,10 @@ d3.json("eventData.json", function(error, data) {
 	.attr("cx", function(d) { return x(d.lng); })
 	.attr("cy", function(d) { return y(d.lat); })
 	.style("fill", function(d) {
+		//Let's color the circles based on the type of event
+		return color(d.code)
+		//Here's a piece of code that colors each event based on when it was last updated
+		/*
 		//Credit for date calculation goes to https://stackoverflow.com/a/3224854/5440366 
 		let now = new Date()
 		let then = new Date(d.lastupdated)
@@ -87,6 +148,8 @@ d3.json("eventData.json", function(error, data) {
 		//console.log(deltaYEars, color(deltaYEars))
 		deltaYEars = deltaYEars > 2 ? 2 : deltaYEars
 		return color(deltaYEars); 
+		*/
+
 	});
 
 
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # This repo functions as an example of how you can load an online JSON file with D3 and how you can visualize it
 
-See an example of hot it work [here](https://razpudding.github.io/fed3-d3events/index.html)
+See an example of how it works [here](https://razpudding.github.io/fed3-d3events/index.html)
 
 ## Data
 The data used comes from the [municipality of Amsterdam](https://data.amsterdam.nl/#?dte=catalogus%2Fapi%2F3%2Faction%2Fpackage_show%3Fid%3Dd7a4c93c-0d7f-4d39-82d4-5f50eaffa624&dtfs=T&dsf=res_format::JSON&mpb=topografie&mpz=11&mpv=52.3731081:4.8932945)
@@ -10,7 +10,7 @@ It includes a list of events and sights which updates
 
 The dataset holds for each event the following information:
 * title 
-* short Description
+* short description
 * lastupdated: Last updated timestamp
 * types: _object_ holding at least one type and category id
 * media: an _array_ holding 0 or more objects whhich hold
